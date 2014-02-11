d813a0f6f12c7997e854be04fc0da9ccd783a02d
Ryo Tulman <ryo.tulman@gmail.com>
2014-02-10 19:31:25 -0800

    Add switch and slider

--- IBOutlets & IBActions/RTViewController.m
+++ IBOutlets & IBActions/RTViewController.m
@@ -13,12 +13,17 @@
 
 @end
 
+NSInteger toggleCount;
+NSInteger logCount;
+NSString *logString;
+
 @implementation RTViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    logCount = 0;
+    toggleCount = 0;
 }
 - (IBAction)goBack:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -32,7 +37,27 @@
     }
 }
 - (IBAction)logToConsole:(id)sender {
-    NSLog(@"Logging to console.");
+    logCount++;
+    logString = [NSString stringWithFormat:@"Log to console count: %ld", (long)logCount];
+    NSLog(logString);
+    [_label setText:logString];
+}
+- (IBAction)toggled:(id)sender {
+    if (toggleCount > 1 && toggleCount%2 == 0) {
+        NSLog(@"Again, eh? It's ok, my CPU is pretty OCD too.");
+    } else if (toggleCount > 0) {
+        NSLog(@"Switched off.");
+    } else {
+        NSLog(@"Switched on.");
+    }
+    toggleCount++;
+}
+- (IBAction)sliderMoved:(UISlider *)sender {
+    if (sender.value > 10) {
+        NSLog(@"It goes up to 11!");
+    } else {
+        NSLog(@"Not 11. What a waste.");
+    }
 }
 
 - (void)viewDidAppear:(BOOL)animated
